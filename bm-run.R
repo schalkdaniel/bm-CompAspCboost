db159c6e2fa6f675a4c8c541aef6c588dccbe4dc
Debian <debian@bigger-benchmarks2.novalocal>
2020-09-18 08:44:46 +0000

    update bm-run

--- bm-run.R
+++ bm-run.R
@@ -4,17 +4,17 @@ source("R/bm-run.R")
 ## Data configurations:
 ## ---------------------------------------------------
 
-#ns = c(5000L, 10000L, 20000L, 50000L, 100000L)
-#ps = c(5L, 10L, 20L, 50L)
-#pnoise_rel = c(0.5, 1, 2, 5)
-#sn_ratio = c(0.1, 1, 10)
-#reps = seq_len(20L)
-
-ns = 5000L
-ps = 5L
-pnoise_rel = c(1, 2)
-sn_ratio = 1
+ns = c(5000L, 10000L, 20000L, 50000L, 100000L)
+ps = c(5L, 10L, 20L, 50L)
+pnoise_rel = c(0.5, 1, 2, 5)
+sn_ratio = c(0.1, 1, 10)
 reps = seq_len(20L)
+
+#ns = 5000L
+#ps = 5L
+#pnoise_rel = c(1, 2)
+#sn_ratio = 1
+#reps = seq_len(20L)
 
 df_configs = expand.grid(n = ns, p = ps, pnoise_rel = pnoise_rel,
   sn_ratio = sn_ratio, rep = reps)
@@ -33,4 +33,4 @@ df_configs$pnoise_rel = NULL
 #bm_dirs = "bm-scripts/optimizer/performance"
 bm_dirs = "bm-scripts/optimizer/memory"
 
-runBM(df_configs, bm_dirs, cores = 2L)
+runBM(df_configs, bm_dirs, cores = 15L)
